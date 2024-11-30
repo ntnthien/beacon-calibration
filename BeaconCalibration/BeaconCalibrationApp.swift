c64b63e1902dd1a8010db3f3865a6b3968b51200
Do <ntnthienlk@gmail.com>
2024-11-30 17:48:36 +0700

    Initial Commit

---
+++ BeaconCalibration/BeaconCalibrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconCalibrationApp.swift
+//  BeaconCalibration
+//
+//  Created by Do Nguyen on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BeaconCalibrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
